ce54498f460eb3d87d0dee8ee317f8dde6315ca0
Anthony Dang <anthonydang30@gmail.com>
2019-05-21 12:58:46 +1000

    Added new commands

--- dangbot.py
+++ dangbot.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 
 from humanfriendly import parse_timespan
+from random import choice
 
 TOKEN = ''
 
@@ -36,12 +37,93 @@ async def on_message(message):
             msg += 'time as 5h or 5m or 5s'
             await client.send_message(message.channel, msg + ' ' + str(error))
 
+    # coin flip
+    elif message.content.startswith('!flipacoin'):
+        coin = ['Heads', 'Tails']
+        msg = choice(coin)
+        await client.send_message(message.channel, msg)
+
+    # roshambo
+    elif message.content.startswith('!roshambo'):
+        result = ['Scissors', 'Paper', 'Rock']
+        computer = choice(result)
+
+        m = message.content.split()
+
+        input = m[1]
+
+        if input == computer:
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nDraw'
+            await client.send_message(message.channel, msg)
+        elif input == 'Rock' and computer == 'Scissors':
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nYou win'
+            await client.send_message(message.channel, msg)
+        elif input == 'Rock' and computer == 'Paper':
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nYou lose'
+            await client.send_message(message.channel, msg)
+        elif input == 'Paper' and computer == 'Rock':
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nYou win'
+            await client.send_message(message.channel, msg)
+        elif input == 'Paper' and computer == 'Scissors':
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nYou lose'
+            await client.send_message(message.channel, msg)
+        elif input == 'Scissors' and computer == 'Paper':
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nYou win'
+            await client.send_message(message.channel, msg)
+        elif input == 'Scissors' and computer == 'Rock':
+            msg = 'You: '
+            msg += input
+            msg += '\nComputer: '
+            msg += computer
+            msg += '\nYou lose'
+            await client.send_message(message.channel, msg)
+        else:
+            msg = '!roshambo Scissors or Paper or Rock'
+            await client.send_message(message.channel, msg)
+
+    # randomiser
+    elif message.content.startswith('!random'):
+        m = message.content.split()
+
+        m.remove('!random')
+
+        try:
+            msg = choice(m)
+            await client.send_message(message.channel, msg)
+
+        except Exception as error:
+            msg = '!random choice1 choice2 etc'
+            await client.send_message(message.channel, msg + ' ' + str(error))
+
     # plugs my soundcloud
     elif 'dang' in message.content:
         msg = 'Follow dang on soundcloud www.soundcloud.com/dang000'
         await client.send_message(message.channel, msg)
 
-# login message
 @client.event
 async def on_ready():
     print('Logged in as')
